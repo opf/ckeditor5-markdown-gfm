b5d41a95c66f154eaa829ba4a601118c3c843d02
Oliver Günther <mail@oliverguenther.de>
2018-02-27 08:10:12 +0100

    Add block_quote handler

--- src/renderer/handlers/index.js
+++ src/renderer/handlers/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable camelcase */
+/* Need snake_case to match nodes in CM AST */
+
 import document from './document';
 import paragraph from './paragraph';
+import block_quote from './block_quote';
 import text from './text';
 import strong from './strong';
 import emph from './emph';
@@ -10,6 +14,7 @@ import heading from './heading';
 export default {
 	document,
 	paragraph,
+	block_quote,
 	text,
 	strong,
 	emph,
